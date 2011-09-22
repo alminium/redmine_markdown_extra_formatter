1ee1db74b091dbff31ed083ae5c0ddb89821d930
mikoto20000 <mikoto20000@gmail.com>
2011-09-22 18:16:02 +0900

    fix jstoolbar's link is broken.

--- app/helpers/redmine_markdown_extra_formatter/helper.rb
+++ app/helpers/redmine_markdown_extra_formatter/helper.rb
@@ -3,14 +3,11 @@ module RedmineMarkdownExtraFormatter
     unloadable
 
     def wikitoolbar_for(field_id)
+      heads_for_wiki_formatter
       url = Engines::RailsExtensions::AssetHelpers.plugin_asset_path('redmine_markdown_extra_formatter', 'help', 'markdown_extra_syntax.html')
       help_link = l(:setting_text_formatting) + ': ' +
         link_to(l(:label_help), url,
         :onclick => "window.open(\"#{url}\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
-
-      javascript_include_tag('jstoolbar/jstoolbar') +
-        javascript_include_tag('markdown_extra', :plugin => 'redmine_markdown_extra_formatter') +
-        javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language}") +
         javascript_tag("var toolbar = new jsToolBar($('#{field_id}')); toolbar.setHelpLink('#{help_link}'); toolbar.draw();")
     end
 
@@ -20,8 +17,17 @@ module RedmineMarkdownExtraFormatter
     end
 
     def heads_for_wiki_formatter
-      stylesheet_link_tag('jstoolbar') +
-        stylesheet_link_tag('markdown_extra', :plugin => 'redmine_markdown_extra_formatter')
+      unless @heads_for_wiki_formatter_included
+        content_for :header_tags do
+          javascript_include_tag('jstoolbar/jstoolbar') +
+          javascript_include_tag('markdown_extra', :plugin => 'redmine_markdown_extra_formatter') +          
+          javascript_include_tag("jstoolbar/lang/jstoolbar-#{current_language}") +
+          stylesheet_link_tag('jstoolbar') +
+          stylesheet_link_tag('markdown_extra', :plugin => 'redmine_markdown_extra_formatter')
+        end
+        @heads_for_wiki_formatter_included = true
+      end
     end
+    
   end
 end
